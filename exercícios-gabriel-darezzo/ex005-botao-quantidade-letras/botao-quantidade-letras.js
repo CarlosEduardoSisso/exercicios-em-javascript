6f64a88f817828f1f4ac0ad276d76b8e81da5cc1
Carlos Eduardo Sisso <carloseduardossz@gmail.com>
2024-10-22 07:43:03 -0300

    Exercício 005 proposto por Gabriel Darezzo concluído
    Concluí o exercício proposto acima.

--- exercícios-gabriel-darezzo/ex005-botao-quantidade-letras/botao-quantidade-letras.js
+++ exercícios-gabriel-darezzo/ex005-botao-quantidade-letras/botao-quantidade-letras.js
@@ -8,3 +8,10 @@ Exemplos de entrada e saida:
 'Dennis Ritchie' // 14 (Espaço conta)
 */
 
+function calcular() {
+    let nome = document.querySelector('input#txtnome');
+    let carac = nome.value.length;
+    let res = document.querySelector('p#resposta');
+    //res.innerHTML = `${nome}`
+    res.innerHTML = `O nome <strong>${nome.value}</strong> tem ${carac} caracteres.`
+}
